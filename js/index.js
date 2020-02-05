8aed5ae65a694aba47499370c1575c393672a5ed
sibrahimpa1 <sibrahimpa1@gmail.com>
2020-02-05 20:25:51 +0100

    Add css changes and videos design

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
 window.onload = function() {
-	$("body").addClass("loaded");
 	sal();
+	$(window).scroll(function (){
+    var content_height = $(document).height();
+    var content_scroll_pos = $(window).scrollTop();
+    var percentage_value = content_scroll_pos * 100 / content_height;
+
+    if(percentage_value === 70)
+    {
+      console.dir("the scroll is about 80% down.");
+			$(".contact div").hide();
+    }
+		else if(percentage_value < 70){
+			$(".contact div").show();
+		}
+	});
 };
--- js/masonary.js
+++ js/masonary.js
@@ -19,6 +19,13 @@ $.getJSON( "./js/data.json", function( data ) {
 			$(".projects").append(projectsList);
 			$(".medias").append(mediasList);
 			$(".videos").append(videosList);
+			$(".lang").on('mouseenter', function(){
+				console.log('lang hover');
+				$(this).closest(".card__top").find(".lang__list").slideToggle();
+			});
+			$(".lang").on('mouseleave', function(){
+				$(".lang__list").hide();
+			});
 			clearInterval(waitContent);
 		}
 	},1);
@@ -29,10 +36,17 @@ function appendListItems(array, order, i, type){
 	let list = '';
 	if(type === "projects"){
 		$.each(array, function( key, val ) {
+			let languages = '';
+			$.each(val.languages, function( key, val ) {
+				languages += `<div class="item">${val}</div>`;
+			});
 			list += `
 				<div class="grid-item span-${order[i]} card" style="background-image: url(${val.imgUrl})" data-sal="slide-up" data-sal-duration="2000" data-sal-easing="ease-out-back">
 					<div class="card__top">
 						<div class="lang">${val.languages.length}</div>
+						<div class="lang__list">
+							${languages}
+						</div>
 					</div>
 					<div class="card__bottom">
 						<a href="${val.projectUrl}" class="title">${val.name}</a>
@@ -45,11 +59,18 @@ function appendListItems(array, order, i, type){
 	}
 	else if(type === "medias"){
 		$.each(array, function( key, val ) {
+			let languages = '';
+			$.each(val.languages, function( key, val ) {
+				languages += `<div class="item">${val}</div>`;
+			});
 			list += `
 				<div class="grid-item span-${order[i]} card" style="background-image: url(${val.imgUrl})" data-sal="fade" data-sal-duration="2000" data-sal-easing="ease-out-back">
 					<div class="card__top">
 						<a href="${val.projectUrl}" class="project-name">${val.projectName}</a>
 						<div class="lang">${val.languages.length}</div>
+						<div class="lang__list">
+							${languages}
+						</div>
 					</div>
 					<div class="card__bottom">
 						<a href="${val.postUrl}" class="title">${val.name}</a>
@@ -63,16 +84,23 @@ function appendListItems(array, order, i, type){
 	}
 	else if(type === "videos"){
 		$.each(array, function( key, val ) {
+			let languages = '';
+			$.each(val.languages, function( key, val ) {
+				languages += `<div class="item">${val}</div>`;
+			});
 			list += `
-				<div class="grid-item span-${order[i]} card" data-sal="slide-up" data-sal-duration="600" data-sal-easing="ease-out-back">
-					<div class="card__top">
+				<div class="media-card" data-sal="slide-up" data-sal-duration="600" data-sal-easing="ease-out-back">
+					<div class="media-card__top card__top">
 						<a href="${val.projectUrl}" class="project-name">${val.projectName}</a>
 						<div class="lang">${val.languages.length}</div>
+						<div class="lang__list">
+							${languages}
+						</div>
 					</div>
-					<div class="card__bottom">
+					<div class="media-card__bottom">
 						<div class="title">${val.name}</div>
 						<div class="desc">${val.description}</div>
-						<a href="${val.mediaUrl}" class="media-name">${val.mediaName}</a>
+						<a href="${val.mediaUrl}" class="media-name"><b>BY:</b> ${val.mediaName}</a>
 					</div>
 				</div>`
 			if(i!=20) i+=1;
